83e7f5d5019fb5b99c72b98088506631fef47750
vijayguntuku <vijay.guntuku@wavemaker.com>
2022-09-07 22:53:31 +0530

    "Overview in html"

--- pttlAuction/JS/setup.js
+++ pttlAuction/JS/setup.js
@@ -1,17 +1,14 @@
 function loadTeams(){
     //alert(new URL(location.href).searchParams.get("catId"));
-        var catId = new URL(location.href).searchParams.get("catId");
-        var url;
-        if(catId!=null && catId!=undefined && catId!="")
-            url = 'pttlAuction/team?id='+catId;
-        else
-            url = 'pttlAuction/teamlist';
+    
+        var url   
+         url = 'pttlAuction/teamlist';
     
         ongoAjaxRequestAsync("GET",url,'', function(res){
             //console.log(res);
             $.each(res.data, function(idx){
             var team = res.data[idx];
-            console.log("prod"+product.name);
+            console.log("prod"+team.name);
             var childli = '<li><figure><a class="aa-product-img" href="#"><img  style ="height:235px;width:285px" alt="product image"></a>'+
              '<a class="aa-add-card-btn" id="addToCart" onclick="addToCart('+product.id+')"><span class="fa fa-shopping-cart"></span>Add To Cart</a>'+
              '<figcaption>'+
